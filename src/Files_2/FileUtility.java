63d553a0b09a32fc96d76fdca7f35c4c81e97650
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-02-14 22:56:19 +0400

    FileWriter (append)

--- src/Files_2/FileUtility.java
+++ src/Files_2/FileUtility.java
@@ -5,9 +5,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileUtility {
-    public static void writeIntoFile(String fileName,String text) throws IOException {
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));){
+    private static void writeIntoFile(String fileName,String text, boolean append) throws IOException {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, append));){
             bw.write(text);
         }
+    }
+
+    public static void writeIntoFile(String fileName,String text) throws IOException {
+        writeIntoFile(fileName,text,false);
+    }
+
+    public static void appendIntoFile(String fileName,String text) throws IOException {
+        writeIntoFile(fileName,text,true);
     }
 }
--- src/Files_2/Main.java
+++ src/Files_2/Main.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileUtility.writeIntoFile("test.txt", "Hello World!");
+        FileUtility.writeIntoFile("test.txt", "Hello World!", true);
     }
 }
