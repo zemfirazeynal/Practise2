6cac81af84a9bcd7d02d485f426b3f0c3512af61
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-02-16 17:51:35 +0400

    FileOutputStream

--- src/Files_2/FileUtility.java
+++ src/Files_2/FileUtility.java
@@ -1,8 +1,6 @@
 package Files_2;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtility {
     private static void writeIntoFile(String fileName,String text, boolean append) throws IOException {
@@ -17,5 +15,15 @@ public class FileUtility {
 
     public static void appendIntoFile(String fileName,String text) throws IOException {
         writeIntoFile(fileName,text,true);
+    }
+
+    public static void writeIntoFile2(String fileName, byte [] data) throws Exception{
+        File file = new File(fileName);
+        FileOutputStream fops = new FileOutputStream(file);
+
+        fops.write(data);
+        fops.flush();
+        fops.close();
+
     }
 }
