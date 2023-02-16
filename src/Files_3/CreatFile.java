0f727e5e0acf794dc9a5f7aded27c5ddd384df2e
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-02-16 20:10:05 +0400

    FileOutputStream

--- src/Files_3/CreatFile.java
+++ src/Files_3/CreatFile.java
@@ -5,14 +5,10 @@ import java.io.File;
 public class CreatFile {
     public static void main(String[] args) {
         try {
-            File f = new File("src/Files_3/input.txt");
+            File f = new File("src/Files_3/output.txt");
             f.createNewFile();
         }catch (Exception e){
             e.printStackTrace();
         }
-
-        /*
-
-         */
     }
 }
--- src/Files_3/FileIOStream.java
+++ src/Files_3/FileIOStream.java
@@ -1,25 +1,36 @@
 package Files_3;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileIOStream {
     public static void main(String[] args) {
 
         try{
-            FileInputStream fins = new FileInputStream("src/Files_3/input.txt");
-            int i = fins.read();
+            FileInputStream fis = new FileInputStream("src/Files_3/input.txt");
+            int i = fis.read();
             while(i!=-1) {
                 System.out.print((char) i);
-                i = fins.read();
+                i = fis.read();
             }
-            fins.close();
-        }catch(Exception e){
+            fis.close();
+        }catch(IOException e){
             e.printStackTrace();
         }
 
+        System.out.println();
         System.out.println("---------------");
 
+        try{
+            String data = "Salam Dunya";
+            FileOutputStream fos = new FileOutputStream("src/Files_3/output.txt");
+            byte [] bytes = data.getBytes(StandardCharsets.UTF_8);
+            fos.write(bytes);
+            fos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
